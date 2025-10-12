31134fca90ebb5c416c5e7c9aa466a9996c39b67
Areefa Samar <samarareefa@gmail.com>
2025-10-12 19:54:32 +0500

    Add files via upload

---
+++ CIP Assignment 07/Number_Pyramid.c
@@ -0,0 +1,23 @@
+// Program to make an upward pyramid of numbers
+#include<stdio.h>
+int main(void)
+{
+	int i, j, space;
+	
+	for ( i=1 ; i <= 5 ; i++)
+	{
+		for ( space=1 ; space <= 5-i ; space++ )
+		{
+		printf(" ");
+	    }
+	    
+	    for( j=1 ; j <= i ; j++)
+	    {
+	    	printf("%d ",j);
+		}
+		
+		printf("\n");
+	}
+	
+	return 0;
+}
